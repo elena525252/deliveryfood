a7860aad8d61166d9c158f5e1cb644d60d951620
elena525252 <90596047+elena525252@users.noreply.github.com>
2021-12-10 14:12:42 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,71 +1,70 @@
 const modal = document.querySelector('.modal');
-const buttonCart = document.querySelector('.button__cart');
-const buttonClosed = document.querySelector('.button-closed');
-const cancelButton = document.getElementById('cancelButton');
-const rows = modal.querySelectorAll('.cart-string');
-const total = modal.querySelector('.fullprice');
+const btnCart = document.getElementById('cart');
+const btnClose = document.querySelector('.btn-close');
+const btnCancel = document.querySelector('.cart-popup__bottom-cancel');
+const rows = modal.querySelectorAll('.row');
+const total = modal.querySelector('.modal-sum');
+
+
+btnCart.addEventListener('click', toggleModal);
+btnClose.addEventListener('click', toggleModal);
+btnCancel.addEventListener('click', toggleModal);
 
 
 
+function toggleModal() {
+    modal.classList.toggle('modal--close')
+};
+
+modal.addEventListener('click', (event)=>{
+	if(event.target == modal){
+		toggleModal()
+	}
+})
 
 
-const toggleModal = function() {
-    modal.classList.toggle('open');
-}
-
-const removeOfModal = function(event) {
-    if (event.target.classList === modal.classList) {
-        modal.classList.remove('open')
-    }
-}
 
 const getFullPrice = () => {
-    let fullprice = 0;
-    const rows = modal.querySelectorAll('.cart-string');
-    rows.forEach(rows => {
-        let newPrice = 0;
-        let priceBlock = rows.querySelector('.price-dish')
-        let price = +priceBlock.textContent
-        fullprice += price;
-    })
-    
-    total.textContent = fullprice
-
+	let fullPrice = 0;
+	const rows = modal.querySelectorAll('.row');
+	rows.forEach(row => {
+		let priceBlock = row.querySelector('.price')
+		let price = +priceBlock.textContent 
+		fullPrice += price;
+	})
+	total.textContent = fullPrice
 }
 
-getFullPrice()
 
-buttonCart.addEventListener('click', toggleModal);
-buttonClosed.addEventListener('click', toggleModal);
-cancelButton.addEventListener('click', toggleModal);
-modal.addEventListener('click', removeOfModal);
+rows.forEach(row => {
+	let newPrice = 0;
+	let priceBlock = row.querySelector('.price')
+	let price = +priceBlock.textContent 
+	let countBlock = row.querySelector('.count')
+	let count = countBlock.textContent
+	const btnMinus = row.querySelector('.btn-count-minus')
+	const btnPlus = row.querySelector('.btn-count-plus')
 
-rows.forEach(rows => {
-    let newPrice = 0;
-    let priceBlock = rows.querySelector('.price-dish')
-    let price = +priceBlock.textContent
-    let countBlock = rows.querySelector('.count')
-    let count = countBlock.textContent
-    const btnminus = rows.querySelector('.minus')
-    const btnplus = rows.querySelector('.plus')
+	const getNewPrice = (count, price) => {
+		newPrice = count * price
+		priceBlock.textContent = newPrice
+		getFullPrice()
+	}
+	
+	btnMinus.addEventListener('click', () => {
+		if (count > 0) {
+			count--
+			countBlock.textContent = count
+			getNewPrice(count, price)
+		}
+	})
 
-    const getNewPrice = (count, price) => {
-        newPrice = count * price
-        priceBlock.textContent = newPrice
-        getFullPrice()
-    } 
-
-    btnminus.addEventListener('click', () => {
-        if (count > 0) {
-            count--
-            countBlock.textContent = count
-            getNewPrice(count, price)
-        }
-    })
-    btnplus.addEventListener('click', () => {
-        count++
-        countBlock.textContent = count
-        getNewPrice(count, price)
-    })
-
+	btnPlus.addEventListener('click', () => {
+		count++
+		countBlock.textContent = count
+		getNewPrice(count, price)
+	})
+	
 })
+
+getFullPrice()
